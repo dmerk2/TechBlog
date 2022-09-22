ab31dabd153a360ff7418df94c846f9c400fc27c
Daniel Merkin <dan.merkin@gmail.com>
2022-09-22 15:47:57 -0400

    creating API route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const userRoutes = require('./user-routes');
+const userRoutes = require("./user-routes");
 
-router.use('/users', userRoutes);
+router.use("/users", userRoutes);
+
+router.use("*", (req, res) => {
+  res.send("<h1>Wrong Route!</h1>");
+});
 
 module.exports = router;
