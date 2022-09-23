556772877414b98aa7aeb9f97f246732a7534418
Daniel Merkin <dan.merkin@gmail.com>
2022-09-23 11:47:46 -0400

    Cleaned up code

--- controllers/api/comments-routes.js
+++ controllers/api/comments-routes.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
-const { Comment, Post } = require('../../models');
+const { Comment, Post } = require("../../models");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Comment.findAll({
-    include: [{model:Comment, through: Post}],
+    include: [{ model: Comment, through: Post }],
   })
-  .then((dbCommentData) => {
-    if (!dbCommentData) {
-      res.status(500).json({ message: "No Comments" });
-      return;
-    }
-    res.json(dbCommentData);
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(500).json({ message: "No Comments" });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get("/:id", (req, res) => {
@@ -24,29 +24,30 @@ router.get("/:id", (req, res) => {
       id: req.params.id,
     },
     include: [Comment, Post],
-  }).then((dbCommentData) => {
-    if (!dbCommentData) {
-      res.status(500).json({ message: "No Comments" });
-      return;
-    }
-    res.json(dbCommentData);
   })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(500).json({ message: "No Comments" });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res) => {
   Comment.create(req.body)
-  .then((comment) => {
-    res.status(200).json(comment);
-  })
-  .then((commentIds) => res.status(200).json(commentIds))
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((comment) => {
+      res.status(200).json(comment);
+    })
+    .then((commentIds) => res.status(200).json(commentIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
@@ -55,14 +56,14 @@ router.delete("/:id", (req, res) => {
       id: req.params.id,
     },
   })
-  .then((data) => {
-    console.log(data)
-    res.status(200).json(data);
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((data) => {
+      console.log(data);
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
